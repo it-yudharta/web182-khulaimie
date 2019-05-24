0839595bde7ea0881325c676c609175b1fc77149
Akhmad Khulaimi <50537349+khulaimie@users.noreply.github.com>
2019-05-25 00:35:47 +0700

    add model and migration

--- database/migrations/2019_05_24_172340_create_bukus_table.php
+++ database/migrations/2019_05_24_172340_create_bukus_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateBukusTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('bukus', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            // $table->string('judul');
+            // $table->string('pengarang');
+            // $table->string('penerbit');
+            // $table->integer('tahun_terbit');
             $table->timestamps();
         });
     }
@@ -31,6 +30,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('bukus');
     }
 }
+
